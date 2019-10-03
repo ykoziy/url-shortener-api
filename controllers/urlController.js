e6c7da6a58c0ad111f0797a1e8e8f1a6508ce0cc
Yuriy Koziy <yukoziy@gmail.com>
2019-10-03 19:23:12 -0400

    Added urlController, not functional yet.

---
+++ controllers/urlController.js
@@ -0,0 +1,19 @@
+const ShortUrl = require('../models/ShortUrl');
+
+exports.addUrl = (req, res) {
+  // get: url, from POST body
+  // check if passed url is valid
+  // If url does not exist add it
+  // otherwise add entry to the DB: {original_url, short_url}
+  // short_url <- auto increment field - {$inc: { short_url: 1}
+  res.send('TODO: addUrl');
+}
+
+exports.getFullUrl = (req, res) {
+  // get: param from req.params.shorturl
+  // check if correct type, a number
+  // for response, redirect to full url
+  res.send('TODO: getFullUrl');
+}
+
+//  original_url: {type: String, required: true},      short_url: {type: Number, default: 1}
