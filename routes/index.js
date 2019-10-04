422b0f6538bb8983bb6eb8ceaf8edbc8a38e4bc7
Yuriy Koziy <yukoziy@gmail.com>
2019-10-03 21:11:15 -0400

    routes/index.js now handles home page request.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router();
 const api = require('./api');
 
+routes.get('/', (req, res) => {
+  res.sendFile('../views/index.html');
+});
+
 routes.use('/api', api);
 
 module.exports = routes;
