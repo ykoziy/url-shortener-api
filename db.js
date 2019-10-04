219316fede3d2af80b6e5299292e7b295ed21ec1
Yuriy Koziy <yukoziy@gmail.com>
2019-10-04 02:25:28 -0400

    Added useFindAndModify: false flag in db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
 
 mongoose.connection.on('connected', () => {
   console.log('Mongoose default connection is open.');
