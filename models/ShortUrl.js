abe4c84ed24c0f5a05899a0258903b252a662707
Yuriy Koziy <yukoziy@gmail.com>
2019-10-04 02:24:07 -0400

    Added AutoIncrement plugin for ShortUrl schema

--- models/ShortUrl.js
+++ models/ShortUrl.js
@@ -3,10 +3,8 @@ const Schema = mongoose.Schema;
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 let ShortUrlSchema = new Schema({
-  original_url: {type: String, required: true},
-  short_url: {type: Number, default: 1}
-
+  original_url: {type: String, required: true}
 });
-ShortUrlSchema.plugin(AutoIncrement, {inc_field: 'id'});
+ShortUrlSchema.plugin(AutoIncrement, {inc_field: 'short_url'});
 let ShortUrl =  mongoose.model('ShortUrl', ShortUrlSchema);
 module.exports = ShortUrl;
