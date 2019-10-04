3e28e7cd321b8a883cabbc1d00d7e74ebbbfb21f
Yuriy Koziy <yukoziy@gmail.com>
2019-10-03 22:27:12 -0400

    Added body-parser

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const port = process.env.PORT;
 const db = require('./db');
 const routes = require('./routes');
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 routes.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
