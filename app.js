86e0e50024f46a401cdfe64b979dde157166bb09
Yuriy Koziy <yukoziy@gmail.com>
2019-10-03 21:12:01 -0400

    Modified app.js to handle routes

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 const db = require('./db');
+const routes = require('./routes');
 
 app.use(express.static(__dirname + '/public'));
 
-//app.use('/', routes);
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.use('/', routes);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
